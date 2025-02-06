64429b330a6b504f388d13f7873e65b9ada2fbdb
erica-co <qkdxks0525@naver.com>
2025-02-06 14:49:07 +0900

    dao

--- src/main/java/com/winter/app/countries/CountryDAO.java
+++ src/main/java/com/winter/app/countries/CountryDAO.java
@@ -1,8 +1,42 @@
 package com.winter.app.countries;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.winter.app.utils.DBConnection;
+
 public class CountryDAO {
 	
-	public void getList() throws Exception {
+	public List<CountryDTO> getList() throws Exception {
+		
+		Connection con = DBConnection.getConnection();
+		String sql = "SELECT * FROM COUNTRIES";
+		PreparedStatement st = con.prepareStatement(sql);
+		List<CountryDTO> ar = new ArrayList<CountryDTO>();
+		ResultSet rs = st.executeQuery();
+		
+		while(rs.next()) {
+			CountryDTO countryDTO = new CountryDTO();
+			countryDTO.setCountry_id(rs.getString("country_id"));
+			countryDTO.setCountry_name(rs.getString("country_name"));
+			countryDTO.setRegion_id(rs.getLong("region_id"));
+			ar.add(countryDTO);
+		}
+		
+		DBConnection.disConnection(rs, st, con);
+		
+		return ar;
+	}
+	
+	public void getDetail() {
+		
+		
+		
+		
+		
 		
 		
 		
